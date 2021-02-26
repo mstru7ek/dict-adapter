c6eb1bb474181d028285a4ba34779ba59502da10
michal struzek <mstruzek@>
2021-02-26 01:20:00 +0100

    valid mail-to

--- src/main/groovy/pl/dev/revelboot/dict/GazetaAdapterServlet.groovy
+++ src/main/groovy/pl/dev/revelboot/dict/GazetaAdapterServlet.groovy
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import pl.dev.revelboot.dict.provider.ContentProviderService
 import pl.dev.revelboot.dict.provider.impl.GazetaProviderService
@@ -20,13 +21,18 @@ class GazetaAdapterServlet {
 
     private ContentProviderService contentProvider = new GazetaProviderService()
 
+    private static final def VALID_TO = ["mmstruzek.atm@gmail.com"]
+
     @GetMapping("/api")
-    def grabContent() {
+    def grabContent(@RequestParam("mailTo") String mailTo) {
         def response = contentProvider.request().get();
 
         def message = "${response.getDate()} ; ${response.getTitle()} ; ${response.getDaily()}"
 
-        SandMail.message("mstru7ek@gmail.com", "gazeta.pl", message);
+        if (VALID_TO.contains(mailTo)) {
+            SandMail.message(mailTo, "gazeta.pl", message);
+        }
+
         return new ResponseEntity<>(response, HttpStatus.OK)
    }
 }
